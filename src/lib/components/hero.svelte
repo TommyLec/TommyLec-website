d77ca6675ba4ff042f099c3075ccae9bd6932d9c
TommyLec <tommy.lecourt@gmail.com>
2025-02-09 23:18:01 +0100

    Fix link 2

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -1,3 +1,7 @@
+<script>
+    import { base } from '$app/paths';
+</script>
+
 <div id="hero" class="flex flex-row justify-center items-center gap-10 my-24">
     <div class="grid gap-4">
         <div>
@@ -5,7 +9,7 @@
             <h2 class=" mt-2">Développeur Web Fullstack | Freelance</h2>
         </div>
 
-        <a class="text-[#009688] px-4 py-2 rounded-md border-3 border-[#009688] hover:bg-[#009688] hover:text-white hover:cursor-pointer w-fit" href="/">Contactez-moi</a>
+        <a class="text-[#009688] px-4 py-2 rounded-md border-3 border-[#009688] hover:bg-[#009688] hover:text-white hover:cursor-pointer w-fit" href="{base}/#contact">Contactez-moi</a>
     </div>
     <img src="./profil-pic.jpg" alt="Tommy" class="rounded-full w-64 h-64 border-3 border-[#009688] p-2" />
 </div>
